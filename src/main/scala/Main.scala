086fe72aea947b66e173b451e0b8a3195b1686b1
aryzach <zach_asmith@yahoo.com>
2021-09-14 14:19:13 -0500

    buy / sell working (with acct allocation)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,12 @@ import com.ib.client.Types.{BarSize, DurationUnit, SecType, WhatToShow}
 import com.tictactec.ta.lib._
 import org.ta4j.core._
 
-import ib._
+import com.larroy.ibclient.{IBClient}
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+
+
+//import ib._
 import contracts._
 import datetime._
 
@@ -16,13 +21,13 @@ object Main {
     import scala.concurrent.duration.Duration
     import com.larroy.ibclient.{IBClient}
 
-    //val ibc = new IBClient("localhost", 7496, 3)
+    val ibc = new IBClient("localhost", 7496, 3)
     //ibc.disconnect()
-    //Await.result(ibc.reconnect(), Duration.Inf) 
+    Await.result(ibc.reconnect(), Duration.Inf) 
 
-    val ibc = IB.connect() 
+    //val ibc = IB.connect() 
 
-    val unqualifiedContract = Contracts.suggestForex()
+    val unqualifiedContract = Contracts.suggestStock()
 
     // not sure if I need but here to remember: ibc.tickPrice and ibc.tickSize
 
@@ -40,19 +45,15 @@ object Main {
     // case class Buy(override val kind: Kind, override val quantity: Int)
     import com.larroy.ibclient.order.{Buy, Sell}
     import com.larroy.ibclient.order.kind.{Limit}
-    val buyOrder = new Buy(new Limit(1.50), 1)
-    val sellOrder = new Sell(new Limit(1.00), 1)
+    val buyOrder = new Buy(new Limit(800), 1, Some("DU3987932"))
+    val sellOrder = new Sell(new Limit(1.00), 1, Some("DU3987932"))
 
     //this is JAVA native API client.placeOrder(nextOrderId++, ContractSamples.USStock(), OrderSamples.LimitOrder("SELL", 1, 50));
     // this is IBClient
-    ibc.placeOrder(unqualifiedContract, buyOrder)
+    ibc.placeOrder(unqualifiedContract, sellOrder)
 
 
     ibc.disconnect()
-
-
-
-
 
   }
 }
